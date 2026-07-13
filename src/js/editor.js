efb855b47944feb04999baf23f1b8b0f0db68789
IdkAnythin07 <IdkAnythin07@proton.me>
2026-07-14 03:55:05 +0530

    Fix: Shit that i broke

--- src/js/editor.js
+++ src/js/editor.js
@@ -1,31 +1,12 @@
 import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
-
-// Import workers
+import "monaco-editor/esm/vs/basic-languages/python/python.contribution";
+import "monaco-editor/esm/vs/basic-languages/cpp/cpp.contribution";
 import editorWorker from "monaco-editor/esm/vs/editor/editor.worker?worker";
-import jsonWorker from "monaco-editor/esm/vs/language/json/json.worker?worker";
-import cssWorker from "monaco-editor/esm/vs/language/css/css.worker?worker";
-import htmlWorker from "monaco-editor/esm/vs/language/html/html.worker?worker";
-import tsWorker from "monaco-editor/esm/vs/language/typescript/ts.worker?worker";
 
 self.MonacoEnvironment = {
     getWorker(_, label) {
-        switch (label) {
-            case "json":
-                return new jsonWorker();
-            case "css":
-            case "scss":
-            case "less":
-                return new cssWorker();
-            case "html":
-            case "handlebars":
-            case "razor":
-                return new htmlWorker();
-            case "typescript":
-            case "javascript":
-                return new tsWorker();
-            default:
-                return new editorWorker();
-        }
+        // Since we aren't doing HTML/CSS/TS, we just return the standard fast worker for everything
+        return new editorWorker();
     }
 };
 
